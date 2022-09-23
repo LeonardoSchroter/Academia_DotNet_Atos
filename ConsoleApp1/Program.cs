4591c6903d033cfd17eb683b896c99784b05d9c4
Leonardo Barros <96589644+LeonardoSchroter@users.noreply.github.com>
2022-09-23 13:43:45 -0300

    exercicios

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*exerxixio1
+            int num1, num2;
+            Console.WriteLine("escreva dois numeros");
+            num1 = int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("media aritmetica entre eles: " + (num1 + num2) / 2);
+            */
+
+
+        }
+    }
+}
