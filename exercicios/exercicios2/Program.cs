924617d57a54f52295dfe2de2a507df532b6a76f
Leonardo Barros <96589644+LeonardoSchroter@users.noreply.github.com>
2022-09-24 10:57:09 -0300

    exercicios

---
+++ exercicios/exercicios2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace exercicios2
+{
+    internal class Program
+    {
+        static void main (string[] args)
+        {
+
+            //exercicio1
+
+           int h,b;
+
+            Console.WriteLine("Qual o tamanho da base do triangulo?");
+            b=int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual o tamanho da altura do triangulo?");
+            h = int.Parse(Console.ReadLine());
+
+
+
+        }
+    }
+}
