827eb59ba43cbbc49e4c389ee00f092ff6b5d88b
Leonardo Barros <96589644+LeonardoSchroter@users.noreply.github.com>
2022-09-26 16:50:42 -0300

    exercicios

--- exercicios/exercicios1/Program.cs
+++ exercicios/exercicios1/Program.cs
@@ -102,151 +102,153 @@
             Console.WriteLine("Custo total dos parafusos A:" + totalA);
             Console.WriteLine("Custo total dos parafusos B:" + totalB);
             */
-        
-
-        /*exercicio7
-        float numero, salario, taxa, vendas, salario_total;
-        Console.WriteLine( "Qual o seu número?");
-        numero = float.Parse(Console.ReadLine());
-        Console.WriteLine( "Qual o seu salario fixo?");
-        salario = float.Parse(Console.ReadLine());
-        Console.WriteLine( "Qual a taxa que ganha sobre vendas em porcentagem?");
-        taxa = float.Parse(Console.ReadLine());
-        Console.WriteLine( "Qual o seu total de vendas?");
-        vendas = float.Parse(Console.ReadLine());
-
-        salario_total= salario+((taxa/100)*vendas);
-        Console.WriteLine("o seu salario eh de "+salario_total);
-        */
-
-        /*exercicio8
-        float c,f;
-        Console.WriteLine("Digite quala temperatura em celcius?");
-        c = float.Parse(Console.ReadLine());
-        f=(9*c+160)/5;
-        Console.WriteLine("a temperatura em farenheit eh igual a: "+f);
-        */
-
-        /*exercicio9
-
-        float valor, entrada , prestacoes;
-        Console.WriteLine("escreva qual o valor da sua compra");
-        valor = float.Parse(Console.ReadLine());
-        if (valor%3!= 0)
-        {
-
-        }
-        */
-
-        /*exercicio10
 
 
-        int valor,nota100=0,nota50=0,nota10=0,nota5=0,nota1=0;
+            /*exercicio7
+            float numero, salario, taxa, vendas, salario_total;
+            Console.WriteLine( "Qual o seu número?");
+            numero = float.Parse(Console.ReadLine());
+            Console.WriteLine( "Qual o seu salario fixo?");
+            salario = float.Parse(Console.ReadLine());
+            Console.WriteLine( "Qual a taxa que ganha sobre vendas em porcentagem?");
+            taxa = float.Parse(Console.ReadLine());
+            Console.WriteLine( "Qual o seu total de vendas?");
+            vendas = float.Parse(Console.ReadLine());
 
-        Console.WriteLine( "qual o valor que deseja sacar?");
-        valor = int.Parse(Console.ReadLine());
-        for (; ; ) { 
+            salario_total= salario+((taxa/100)*vendas);
+            Console.WriteLine("o seu salario eh de "+salario_total);
+            */
 
-            if (valor == 0) {
-                break;
+            /*exercicio8
+            float c,f;
+            Console.WriteLine("Digite quala temperatura em celcius?");
+            c = float.Parse(Console.ReadLine());
+            f=(9*c+160)/5;
+            Console.WriteLine("a temperatura em farenheit eh igual a: "+f);
+            */
+
+            /*exercicio9
+
+            Console.WriteLine("Informe o valor da mercadoria");
+                double mercadoria = double.Parse(Console.ReadLine());
+                double prestacao = 0;
+                double resto = mercadoria % 3;
+                mercadoria = mercadoria - resto;
+                prestacao = mercadoria / 3;
+                double entrada = prestacao + resto;
+                Console.WriteLine("entrade de " + entrada + " e 2x de " + prestacao);
+            */
+            
+
+            /*exercicio10
+
+
+            int valor,nota100=0,nota50=0,nota10=0,nota5=0,nota1=0;
+
+            Console.WriteLine( "qual o valor que deseja sacar?");
+            valor = int.Parse(Console.ReadLine());
+            for (; ; ) { 
+
+                if (valor == 0) {
+                    break;
+                }
+
+                if (valor >= 100){
+                    nota100++;
+                    valor=valor-100;
+                    continue;
+                }
+
+                if (valor >= 50)
+                {
+                    nota50++;
+                    valor=valor-50;
+                    continue;
+                }
+
+                if (valor >= 10)
+                {
+                    nota10++;
+                    valor=valor-10;
+                    continue;
+                }
+
+                if (valor >= 5)
+                {
+                    nota5++;
+                    valor=valor-5;
+                    continue;
+                }
+
+                if (valor >= 1)
+                {
+                    nota1++;
+                    valor=valor-1;
+                    continue;
+                }
             }
 
-            if (valor >= 100){
-                nota100++;
-                valor=valor-100;
-                continue;
-            }
-
-            if (valor >= 50)
+            if(nota100 != 0)
             {
-                nota50++;
-                valor=valor-50;
-                continue;
+                Console.WriteLine(nota100+ " notas de 100");
             }
-
-            if (valor >= 10)
+            if(nota50 != 0)
             {
-                nota10++;
-                valor=valor-10;
-                continue;
+                Console.WriteLine(nota50+ " notas de 50");
             }
-
-            if (valor >= 5)
+            if(nota10 != 0)
             {
-                nota5++;
-                valor=valor-5;
-                continue;
+                Console.WriteLine(nota10+ " notas de 10");
             }
-
-            if (valor >= 1)
+            if(nota5 != 0)
             {
-                nota1++;
-                valor=valor-1;
-                continue;
+                Console.WriteLine(nota5+ " notas de 5");
             }
+            if(nota1 != 0)
+            {
+                Console.WriteLine(nota1+ " notas de 1");
+            }
+            */
+
+
+            /*exercicio11
+
+            int vBrancos, vNulos, vValidos, total, pBrancos, pNulos, pValidos;
+
+            Console.WriteLine("escreva qual foi o total de votos");
+            total = int.Parse(Console.ReadLine());
+            Console.WriteLine("escreva qual foi o total de votos em branco");
+            vBrancos = int.Parse(Console.ReadLine());
+            Console.WriteLine("escreva qual foi o total de votos nulos");
+            vNulos = int.Parse(Console.ReadLine());
+            Console.WriteLine("escreva qual foi o total de votos validos");
+            vValidos = int.Parse(Console.ReadLine());
+
+            pValidos = (vValidos * 100) / total;
+            pBrancos = (vBrancos * 100) / total;
+            pNulos = (vNulos * 100) / total;
+
+            Console.WriteLine(pValidos + "% foram validos\n" +pBrancos + "% foram em branco\n" + pNulos +"% foram nulos");
+
+            */
+
+            /*exercicio12
+
+            float combustivel = 6.9, kmi, kmf, kmTotal, litros, valor, valortL;
+
+            Console.WriteLine("quantos litros de combustivel voce gastou?");
+            litros = float.Parse(Console.ReadLine());
+            Console.WriteLine("quanto o odometro marcava no inicio do dia?");
+            kmi = float.Parse(Console.ReadLine());
+            Console.WriteLine("quanto o odometro marcava no final do dia?");
+            kmf = float.Parse(Console.ReadLine());
+            Console.WriteLine("quanto voce recebeu dos passageiros?");
+            valor = float.Parse(Console.ReadLine());
+
+            */
+
+
+
         }
-
-        if(nota100 != 0)
-        {
-            Console.WriteLine(nota100+ " notas de 100");
-        }
-        if(nota50 != 0)
-        {
-            Console.WriteLine(nota50+ " notas de 50");
-        }
-        if(nota10 != 0)
-        {
-            Console.WriteLine(nota10+ " notas de 10");
-        }
-        if(nota5 != 0)
-        {
-            Console.WriteLine(nota5+ " notas de 5");
-        }
-        if(nota1 != 0)
-        {
-            Console.WriteLine(nota1+ " notas de 1");
-        }
-        */
-
-
-        /*exercicio11
-
-        int vBrancos, vNulos, vValidos, total, pBrancos, pNulos, pValidos;
-
-        Console.WriteLine("escreva qual foi o total de votos");
-        total = int.Parse(Console.ReadLine());
-        Console.WriteLine("escreva qual foi o total de votos em branco");
-        vBrancos = int.Parse(Console.ReadLine());
-        Console.WriteLine("escreva qual foi o total de votos nulos");
-        vNulos = int.Parse(Console.ReadLine());
-        Console.WriteLine("escreva qual foi o total de votos validos");
-        vValidos = int.Parse(Console.ReadLine());
-
-        pValidos = (vValidos * 100) / total;
-        pBrancos = (vBrancos * 100) / total;
-        pNulos = (vNulos * 100) / total;
-
-        Console.WriteLine(pValidos + "% foram validos\n" +pBrancos + "% foram em branco\n" + pNulos +"% foram nulos");
-
-        */
-
-        /*exercicio12
-
-        float combustivel = 6.9, kmi, kmf, kmTotal, litros, valor, valortL;
-
-        Console.WriteLine("quantos litros de combustivel voce gastou?");
-        litros = float.Parse(Console.ReadLine());
-        Console.WriteLine("quanto o odometro marcava no inicio do dia?");
-        kmi = float.Parse(Console.ReadLine());
-        Console.WriteLine("quanto o odometro marcava no final do dia?");
-        kmf = float.Parse(Console.ReadLine());
-        Console.WriteLine("quanto voce recebeu dos passageiros?");
-        valor = float.Parse(Console.ReadLine());
-
-        */
-
-
-
-    }
     }
 }
