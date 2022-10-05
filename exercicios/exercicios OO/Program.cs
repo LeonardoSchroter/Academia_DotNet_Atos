467e44a7c73cffa5d2091f3bbb37bc0c81edd6f2
Leonardo Barros <96589644+LeonardoSchroter@users.noreply.github.com>
2022-10-05 16:05:08 -0300

    Exemplos orientação a objetos

---
+++ exercicios/exercicios OO/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace exercicios_OO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ orientação a objeto/Orientação a objeto1/Program.cs
@@ -0,0 +1,108 @@
+﻿namespace Orientação_a_objeto1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Ola");
+           
+            //Console é uma classe nativa de C#
+            //WriteLine() é um método de classe da classe console
+
+            string frase = Console.ReadLine();
+
+            //frase é um objeto da classe string, que encapsula/recebe atributos e métodos
+            //da classe strring
+
+            int tamanhoFrase = frase.Length;
+            frase = frase.ToUpper();
+
+            //Length é um atributo de instância da classe string.
+            //ToUpper() é um método de instância
+
+            Random gerador = new Random(); //o método construtor da classe Random é chamado
+                                           //o construtor aloca memória para o objeto gerador
+
+
+            int numeroQualquer = gerador.Next();
+            //next é um método de instância da classe random com retorno
+            //next possui 3 opcoes de comportamento/funcionalidade-Polimorfismo
+            Console.WriteLine(numeroQualquer);
+            */
+
+            ////List<string> nomePessoas = new List<string>(); //construtor que instância o objeto nomePessoas em memnória
+
+
+            //nomePessoas.Add("Rafael Gioffi");
+            //nomePessoas.Add("Isabela da Silva");
+            //nomePessoas.Add("Kaue de Oliveira Victorio");
+
+            //Add é um método de instancia nomePessoas que insere uma string no final da lista
+
+            //for(int i = 0; i < nomePessoas.Count; i++)
+            //{
+            //    Console.WriteLine(nomePessoas[i].ToUpper());
+            //}
+
+            //////count é um método de instância que sabe o tamanho da lista até o momento
+
+
+            ////string nome;
+            ////nomePessoas.Clear();
+            ////do
+            ////{
+            ////    Console.WriteLine("Digite um nome");
+            ////    nome = Console.ReadLine().ToUpper();
+
+
+            ////    if (nomePessoas.Contains(nome))
+            ////    {
+            ////        Console.WriteLine("Nome ja cadastrado");
+            ////    }
+            ////    if(nome == "SAIR")
+            ////    {
+            ////        break;
+            ////    }
+            ////    else
+            ////    {
+            ////        nomePessoas.Add(nome);
+
+            ////    }
+
+            ////}while(true);
+
+            ////Console.WriteLine("Tamanho da lista: "+ nomePessoas.Count);
+            ////nomePessoas.Remove("AlEXANDRE");
+
+            ////Console.WriteLine("Tamanho da lista após remoção: " + nomePessoas.Count);
+            ////for(int i=0; i < nomePessoas.Count; i++)
+            ////{
+            ////    Console.WriteLine(nomePessoas[i]);
+            ////}
+
+            //métodos e atributos da classe list
+            //Add()
+            //Clear()
+            //Contains()
+            //Count
+            //Remove()
+            //Sort()
+
+            //Métodos e atributos da classe string
+            //Length
+            //Contains()
+            //Replace()
+            //String.isNullOrWhiteSpace()
+            //Split
+
+            //exemplo de split
+
+            //string frase = "bom dia a todos";
+            //string[] palavrasFrase = frase.Split(" ");
+
+            //Console.WriteLine(palavrasFrase[2]);
+
+        }
+    }
+}
