d01c596ad60988e028f3c95923ee4fb9d0258ead
Leonardo Barros <96589644+LeonardoSchroter@users.noreply.github.com>
2022-10-13 17:23:59 -0300

    exemplos

---
+++ orientação a objeto/Lists/Pessoas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    internal class Pessoas
+    {
+        public Pessoas() { }
+        public string Nome {get;set; }
+        public int Idade {get;set; }
+
+        public Pessoas(string nome, int idade)
+        {
+            this.Nome = nome;
+            this.Idade = idade;
+        }
+    }
+}
---
+++ orientação a objeto/Lists/Program.cs
@@ -0,0 +1,81 @@
+﻿
+using Lists;
+
+internal class Program
+{
+    static List<Pessoas> pessoas;
+    static void ListaNaoOrdenada()
+    {
+        Console.WriteLine("Lista não ordenada");
+        pessoas.ForEach(delegate (Pessoas p)
+        {
+            Console.WriteLine(p.Idade + " " + p.Nome);
+
+        });
+    }
+    static void ListaOrdenadaPorNome()
+    {
+        Console.WriteLine("Lista ordenada por nome");
+        pessoas.Sort(delegate (Pessoas p1, Pessoas p2)
+        {
+            return p1.Nome.CompareTo(p2.Nome);
+        });
+        pessoas.ForEach(delegate (Pessoas p)
+        {
+            Console.WriteLine(p.Idade + " " + p.Nome);
+        });
+    }
+    static void ListaOrdenadaPorIdade()
+    {
+        Console.WriteLine("Lista ordenada por idade");
+        pessoas.Sort(delegate (Pessoas p1, Pessoas p2)
+        {
+            return p1.Idade.CompareTo(p2.Idade);
+        });
+        pessoas.ForEach(delegate (Pessoas p)
+        {
+            Console.WriteLine(p.Idade + " " + p.Nome);
+        });
+    }
+
+    static void ListaInserirItemNaPosicao()
+    {
+        Console.WriteLine("Inserindo uma pessoa na posição 1 e outra na 3");
+        pessoas.Insert(1, new Pessoas() { Nome = "Bob Dylan", Idade = 78 });
+        pessoas.Insert(1, new Pessoas() { Nome = "Jimmy Page", Idade = 81 });
+
+    }
+    static void ListaLocalizaPessoaMaisJovem()
+    {
+        List<Pessoas> jovem = pessoas.FindAll(delegate (Pessoas p) { return p.Idade < 45; });
+
+        Console.WriteLine("Idade é menor que 45 :");
+        jovem.ForEach(delegate (Pessoas p)
+        {
+            Console.WriteLine(p.Nome + " " + p.Idade);
+        });
+    }
+
+    private static void Main(string[] args)
+    {
+        pessoas = new List<Pessoas>();
+        pessoas.Add(new Pessoas("Leonardo" , 40));
+        pessoas.Add(new Pessoas("Fernanda", 30));
+        pessoas.Add(new Pessoas("Marcio", 20));
+
+        foreach (Pessoas p in pessoas)
+        {
+            Console.WriteLine(p.Nome + " " + p.Idade);
+
+        }
+        Console.ReadKey();
+
+        ListaNaoOrdenada();
+        Console.ReadKey();
+        ListaOrdenadaPorNome();
+        Console.ReadKey();
+        ListaOrdenadaPorIdade();
+        Console.ReadKey();
+        ListaInserirItemNaPosicao();
+    }
+}
