76f4d01af4f8112f9c38abb99baa1ed2f496c081
Leonardo Barros <96589644+LeonardoSchroter@users.noreply.github.com>
2022-10-13 16:00:07 -0300

    Exemplos interface

--- orientação a objeto/Interface/IEletronico.cs
+++ orientação a objeto/Interface/IEletronico.cs
@@ -6,10 +6,12 @@ using System.Threading.Tasks;
 
 namespace Interface
 {
-    internal interface Eletronico
+    internal interface IEletronico
     {
-        private int _volts;
-        private int _potencia;
-        private bool _ligado;
+
+        public void Ligar();
+
+        public void Desligar();
+
     }
 }
